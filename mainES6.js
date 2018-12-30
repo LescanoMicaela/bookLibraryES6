16e21ecaf3e552fd768b41248aecff91b778e2e3
Micaela Lescano <lescano.micaela@gmail.com>
2018-12-30 14:36:46 +0100

    isotope function needs to be refactored.

--- mainES6.js
+++ mainES6.js
@@ -12,34 +12,88 @@ getData(url)
 		books = response.books;
 		createFlippers(books);
 		hideElement(loader);
+	
+	var qsRegex;
+
+         // init Isotope
+         var $grid = $("#library").isotope({
+             itemSelector: '.flip-container',
+             layoutMode: 'fitRows',
+             filter: function () {
+                 return qsRegex ? $(this).text().match(qsRegex) : true;
+
+
+             }
+         });
+
+         // use value of search field to filter
+         var $quicksearch = $('.quicksearch').keyup(debounce(function () {
+             qsRegex = new RegExp($quicksearch.val(), 'gi');
+             $grid.isotope();
+
+             if (!$grid.data('isotope').filteredItems.length) {
+                 $('#noResult').show();
+             } else {
+                 $('#noResult').hide();
+             }
+         }, 200));
+
+         // debounce so filtering doesn't happen every millisecond
+         function debounce(fn, threshold) {
+             var timeout;
+             return function debounced() {
+                 if (timeout) {
+                     clearTimeout(timeout);
+                 }
+
+                 function delayed() {
+                     fn();
+                     timeout = null;
+                 }
+                 timeout = setTimeout(delayed, threshold || 100);
+             }
+         }
+
 	})
 	.catch(error => console.error(error))
 
-const hideElement = (el) => el.style.display = "none";
+const hideElement = el => el.style.display = "none";
 
 function createFlippers(arr) {
 	arr.map((book) => {
-		console.log(book);
-		let divContainer = document.createElement("div");
-		divContainer.setAttribute("class", "flip-container");
+		//		console.log(book);
+		templateFilpperBook(book);
 
-		let divFlipper = document.createElement("div");
-		divFlipper.setAttribute("class", "flipper");
-		let front = document.createElement("div");
-		front.setAttribute("class", "front");
-		
-		//using template literals
-		front.innerHTML =
-			`<img src=${book.portada} class="img-repsonsive" />`
-		let back = document.createElement("div");
-		back.setAttribute("class", "back");
-		back.innerHTML =
-			`<h3>${book.titulo}</h3>
-			<p>${book.descripcion}</p>
-			<button class="btn">More info</button>`
-		divContainer.appendChild(divFlipper);
-		divFlipper.appendChild(front);
-		divFlipper.appendChild(back);
-		containerDiv.appendChild(divContainer);
 	})
 }
+
+const templateFilpperBook = book => {
+	//assigment destructuring
+	let {portada,titulo,descripcion} = book;
+	
+	let flipperContainer = document.createElement("div");
+	flipperContainer.setAttribute("class", "flip-container");
+
+	let divFlipper = document.createElement("div");
+	divFlipper.setAttribute("class", "flipper");
+	let front = document.createElement("div");
+	front.setAttribute("class", "front");
+
+	//using template literals
+	front.innerHTML =
+		`<img src=${portada} class="img-repsonsive" />`
+	let back = document.createElement("div");
+	back.setAttribute("class", "back");
+	back.innerHTML =
+		`<h3>${titulo}</h3>
+			<p>${descripcion}</p>
+			<button class="btn">More info</button>`
+	flipperContainer.appendChild(divFlipper);
+	divFlipper.appendChild(front);
+	divFlipper.appendChild(back);
+	containerDiv.appendChild(flipperContainer);
+}
+
+
+
+ 
